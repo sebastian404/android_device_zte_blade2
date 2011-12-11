cdcb65cbc85d234be1759f1fb21e6634dcd361dc
Sebastian Robinson <sebastian.robinson@podtwo.com>
2011-12-11 11:51:30 -0800

    Updated to kernel without FTM mode

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/zte/skate/AndroidBoardVendor.mk
+-include vendor/zte/blade2/AndroidBoardVendor.mk
--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -62,7 +62,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_VOLUMEDOWN:
                 return HIGHLIGHT_DOWN;
 
-            case KEY_SLEEP:
+            case KEY_POWER:
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
